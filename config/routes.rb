a352cd9190dc2c3822490f318ceea154cf038bcf
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-02-23 15:42:18 +0100

    navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
+  get 'about', to: 'pages#about', as: :about
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
