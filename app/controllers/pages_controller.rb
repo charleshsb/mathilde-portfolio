61e1868f65ae7ab7dae399172abbcbda49ece591
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-03-05 17:33:13 +0100

    project example

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def ph
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
   get 'about', to: 'pages#about', as: :about
+  get 'commercial-des-petits-hauts', to: 'pages#ph', as: :ph
 
   get "*missing" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
