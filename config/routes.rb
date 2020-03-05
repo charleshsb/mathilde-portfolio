0899128931f2bedadfee778d7d5a6cdabbe7a6f0
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-03-05 15:43:50 +0100

    images

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
   get 'about', to: 'pages#about', as: :about
 
+  get "*missing" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
