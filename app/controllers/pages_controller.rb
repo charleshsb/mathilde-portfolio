9c41a21f7cce4e5ed29c163e3a788c252ae74432
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-03-06 17:34:36 +0100

    projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,40 @@ class PagesController < ApplicationController
 
   def ph
   end
+
+  def rd
+  end
+
+  def feeka
+  end
+
+  def holiday
+  end
+
+  def jane7
+  end
+
+  def jane6
+  end
+
+  def luco
+  end
+
+  def lv
+  end
+
+  def md
+  end
+
+  def morgan
+  end
+
+  def e_suncoo
+  end
+
+  def l_suncoo
+  end
+
+  def vakko
+  end
 end
